b26aacde9a799f06f6cd7998f5441bee8ebb7672
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-27 22:28:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  attr_accessor :school_name, :roster, :grade, :name
+    
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student (name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+    
+end
